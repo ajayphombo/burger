67ddc6b1cf315c378e39ff1d0879776b54be5869
ajayphombo <sagarsu@live.com>
2019-05-02 10:47:55 -0700

    updated connection with credentials

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,13 @@ var keys= require("../keys");
 var connection;
 
 if (process.env.JAWSDB_URL){
-  connection =mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
 connection= mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password:""+ keys.dbPassword,
+  password:"password",
   database: "burgers_db"
 });
 
